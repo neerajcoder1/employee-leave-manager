4bbfaf78ebcfee3663214eed441fe55532b5cc80
Neeraj Gahlout <neerajgahlout36@gmail.com>
2026-08-03 21:14:16 +0530

    style: adjust logo size to 60x60px

--- frontend/src/pages/EmployeeDashboard.jsx
+++ frontend/src/pages/EmployeeDashboard.jsx
@@ -530,7 +530,7 @@ const EmployeeDashboard = () => {
           <img
             src="/zollid-logo.png"
             alt="Zollid Logo"
-            style={{ height: '80px', width: '80px', objectFit: 'cover' }}
+            style={{ height: '60px', width: '60px', objectFit: 'cover' }}
           />
           <span className="navbar-logo-text">Zollid</span>
         </div>
